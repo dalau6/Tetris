6291f334413ac7ea4c9875701dbd95f12b59266c
David <dalau6@gmail.com>
2019-12-05 20:41:46 -0800

    draw board.

--- piece.js
+++ piece.js
@@ -23,3 +23,18 @@ class Piece {
     this.y = 0;
   }
 }
+
+draw() {
+    this.ctx.fillStyle = this.color;
+    this.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        // this.x, this.y gives the left upper position of the shape
+        // x, y gives the position of the block in the shape
+        // this.x + x is then the position of the block on the board
+        if (value > 0) {
+          this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+        }
+      });
+    });
+  }
+  
