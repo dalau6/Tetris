c39449d8d100fb4692d4aaedd52acfb7e482ecec
David <dalau6@gmail.com>
2019-12-06 23:13:18 -0800

    updated piece.

--- piece.js
+++ piece.js
@@ -4,36 +4,43 @@ class Piece {
   color;
   shape;
   ctx;
-  
+  typeId;
+
   constructor(ctx) {
     this.ctx = ctx;
     this.spawn();
   }
-  
+
   spawn() {
-    this.color = 'blue';
-    this.shape = [
-      [2, 0, 0], 
-      [2, 2, 2], 
-      [0, 0, 0]
-    ];
-    
-    // Starting position.
-    this.x = 3;
+    this.typeId = this.randomizeTetrominoType(COLORS.length - 1);
+    this.shape = SHAPES[this.typeId];
+    this.color = COLORS[this.typeId];
+    this.x = 0;
     this.y = 0;
   }
 
-draw() {
+  draw() {
     this.ctx.fillStyle = this.color;
     this.shape.forEach((row, y) => {
       row.forEach((value, x) => {
-        // this.x, this.y gives the left upper position of the shape
-        // x, y gives the position of the block in the shape
-        // this.x + x is then the position of the block on the board
         if (value > 0) {
           this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
         }
       });
     });
+  }
+
+  move(p) {
+    this.x = p.x;
+    this.y = p.y;
+    this.shape = p.shape;
+  }
+
+  setStartingPosition() {
+    this.x = this.typeId === 4 ? 4 : 3;
+  }
+
+  randomizeTetrominoType(noOfTypes) {
+    return Math.floor(Math.random() * noOfTypes + 1);
   }
 }
